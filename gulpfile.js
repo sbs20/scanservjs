9a9334ec0e6be5c889992ea8f10459dec7ac79c7
sbs20 <sam.strachan@gmail.com>
2016-12-05 16:24:36 +0000

    Improving gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var zip = require('gulp-zip');
 var browserify = require('browserify');
 var browserifyshim = require('browserify-shim');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var del = require('del');
+var dateFormat = require('dateformat');
 
 // See recipes: https://github.com/gulpjs/gulp/tree/master/docs/recipes
 
@@ -22,18 +26,19 @@ gulp.task('js', function () {
 
     return b.bundle()
         .pipe(source('app.js'))
+        .pipe(buffer()) // convert from streaming to buffered vinyl file object
+        .pipe(uglify()) // now gulp-uglify works 
         .pipe(gulp.dest('./assets/js/'));
 });
 
 gulp.task('css', function () {
     return gulp.src([
-            './node_modules/jqueryui/**/*.min.css',
-            './node_modules/jquery-jcrop/**/*.min.css',
-            './node_modules/toastr/**/*.min.css',
-            './node_modules/bootstrap/dist/**/*.min.css',
-            './src/client.css'
-        ])
-        .pipe(concat('app.css'))
+        './node_modules/jqueryui/**/*.min.css',
+        './node_modules/jquery-jcrop/**/*.min.css',
+        './node_modules/toastr/**/*.min.css',
+        './node_modules/bootstrap/dist/**/*.min.css',
+        './src/client.css'
+    ]).pipe(concat('app.css'))
         .pipe(gulp.dest('./assets/css'));
 });
 
@@ -51,21 +56,32 @@ gulp.task('compile', ['files', 'css', 'js']);
 
 gulp.task('build', ['compile'], function () {
     return gulp.src([
-            './index.html',
-            './install.sh',
-            './package.json',
-            './npm-shrinkwrap.json',
-            './scanservjs.service',
-            './server.js',
-            './*assets/**/*',
-            './*classes/**/*',
-            './*data/**/*.md',
-        ])
-        .pipe(gulp.dest('./build'));
+        './index.html',
+        './install.sh',
+        './package.json',
+        './npm-shrinkwrap.json',
+        './scanservjs.service',
+        './server.js',
+        './*assets/**/*',
+        './*classes/**/*',
+        './*data/**/*.md',
+    ]).pipe(gulp.dest('./build'));
+});
+
+gulp.task('package', ['build'], function () {
+    var filename = 'scanservjs_' + dateFormat(new Date(), 'yyyymmdd.HHMMss') + '.zip';
+    return gulp.src('./build/**/*')
+        .pipe(zip(filename))
+        .pipe(gulp.dest('./release'));
 });
 
 gulp.task('rebuild', ['clean'], function () {
-    
+
+});
+
+gulp.task('deploy', ['build'], function () {
+    return gulp.src('./build/**/*.*')
+        .pipe(gulp.dest('//storage/public/scanservjs'));
 });
 
 gulp.task('default', [
