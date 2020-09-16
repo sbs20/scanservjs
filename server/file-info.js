1bd6c73533485cdb7deafc84f6286e9a2f8fcf6c
sbs20@omicron.sbs20.com <sbs20@omicron.sbs20.com>
2020-09-16 11:24:25 +0100

    linting

--- server/file-info.js
+++ server/file-info.js
@@ -15,15 +15,15 @@ const sizeString = (size) => {
   const kb = 1 << 10;
   const mb = kb << 10;
   if (size < 0) {
-      return "0";
-  } else if (size == 1) {
-      return "1 Byte";
+    return '0';
+  } else if (size === 1) {
+    return '1 Byte';
   } else if (size < kb << 1) {
-      return `${size} Bytes`;
+    return `${size} Bytes`;
   } else if (size < mb << 1) {
-      return `${Math.round(size / kb)} KB`;
+    return `${Math.round(size / kb)} KB`;
   } else {
-      return `${Math.round(100.0 * size / mb) / 100.0} MB`;
+    return `${Math.round(100.0 * size / mb) / 100.0} MB`;
   }
 };
 
