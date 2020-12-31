837dafecc031cd68322de0631421ca1ca17e4630
sbs20 <sam.strachan@gmail.com>
2020-12-31 15:38:22 +0000

    Custom docker delimiter #105

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -xve
 
+# Test using the following form:
+# export SANED_NET_HOSTS="a|b" AIRSCAN_DEVICES="c|d" DELIMITER="|"; ./run.sh
+
 # turn off globbing
 set -f
 
@@ -8,9 +11,12 @@ set -f
 IFS='
 '
 
+# Get a custom delimiter but default to ;
+DELIMITER=${DELIMITER:-;}
+
 # Insert a list of net hosts
 if [ ! -z "$SANED_NET_HOSTS" ]; then
-  hosts=$(echo $SANED_NET_HOSTS | sed "s/;/\n/")
+  hosts=$(echo $SANED_NET_HOSTS | sed "s/$DELIMITER/\n/")
   for host in $hosts; do
     echo $host >> /etc/sane.d/net.conf
   done
@@ -18,7 +24,7 @@ fi
 
 # Insert airscan devices
 if [ ! -z "$AIRSCAN_DEVICES" ]; then
-  devices=$(echo $AIRSCAN_DEVICES | sed "s/;/\n/")
+  devices=$(echo $AIRSCAN_DEVICES | sed "s/$DELIMITER/\n/")
   for device in $devices; do
     sed -i "/^\[devices\]/a $device" /etc/sane.d/airscan.conf
   done
