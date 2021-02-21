93d9b48b76e1a3693d605f6e8ec64f0f0be8967b
sbs20 <sam.strachan@gmail.com>
2021-02-21 12:44:56 +0000

    Clear preview

--- webui/src/components/Scan.vue
+++ webui/src/components/Scan.vue
@@ -50,7 +50,7 @@
 
         <div class="d-flex flex-row-reverse flex-wrap">
           <v-btn color="green" @click="createPreview" class="ml-1 mb-1">preview <v-icon class="ml-2">mdi-magnify</v-icon></v-btn>
-          <v-btn color="secondary" @click="deletePreview" class="ml-1 mb-1">delete preview <v-icon class="ml-2">mdi-delete</v-icon></v-btn>
+          <v-btn color="amber" @click="deletePreview" class="ml-1 mb-1">clear <v-icon class="ml-2">mdi-delete</v-icon></v-btn>
           <v-btn color="primary" @click="scan(1)" class="ml-1 mb-1">scan <v-icon class="ml-2">mdi-camera</v-icon></v-btn>
           <v-btn color="secondary" @click="reset" class="ml-1 mb-1">reset <v-icon class="ml-2">mdi-refresh</v-icon></v-btn>
         </div>
