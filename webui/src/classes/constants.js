7a569a1adfb8ae57c853c13dbd3377c38535bb3a
sbs20 <sam.strachan@gmail.com>
2021-05-05 10:26:21 +0100

    Core changes for configurable app colour  #231

--- webui/src/classes/constants.js
+++ webui/src/classes/constants.js
@@ -24,6 +24,29 @@ const Constants = {
     Light: 'light',
     System: 'system'
   },
+
+  Colors: [
+    'accent-4',
+    'red',
+    'pink',
+    'purple',
+    'deep-purple',
+    'indigo',
+    'blue',
+    'light-blue',
+    'cyan',
+    'teal',
+    'green',
+    'light-green',
+    'lime',
+    'yellow',
+    'amber',
+    'orange',
+    'deep-orange',
+    'brown',
+    'blue-grey',
+    'grey'
+  ]
 };
 
 export default Constants;
--- webui/src/components/Settings.vue
+++ webui/src/components/Settings.vue
@@ -16,6 +16,17 @@
         </settings-item>
         <settings-item>
           <template v-slot:description>
+            {{ $t('settings.color:description') }}
+          </template>
+          <template v-slot:action>
+            <div style="max-width: 10rem;">
+              <v-select :label="$t('settings.color')" v-model="settings.appColor" @change="reload"
+                 :items="colors" item-text="text" item-value="value"></v-select>
+            </div>
+          </template>
+        </settings-item>
+        <settings-item>
+          <template v-slot:description>
             {{ $t('settings.locale:description') }}
           </template>
           <template v-slot:action>
@@ -54,21 +65,22 @@ export default {
   },
 
   computed: {
+    colors() {
+      return Constants.Colors.map(c => {
+        return {
+          text: this.$t(`colors.${c}`),
+          value: c
+        };
+      });
+    },
+
     themes() {
-      return [
-        {
-          text: this.$t('settings.theme:system'),
-          value: Constants.Themes.System
-        },
-        {
-          text: this.$t('settings.theme:light'),
-          value: Constants.Themes.Light
-        },
-        {
-          text: this.$t('settings.theme:dark'),
-          value: Constants.Themes.Dark
-        }
-      ];
+      return Object.keys(Constants.Themes).map(t => {
+        return {
+          text: this.$t(`settings.theme:${Constants.Themes[t]}`),
+          value: Constants.Themes[t]
+        };
+      });
     }
   },
 
