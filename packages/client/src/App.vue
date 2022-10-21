6fce6bf70fd5b8f59654990ab625573232498d73
Leo Wehrfritz <leocommander.cs@gmail.com>
2022-10-21 18:46:07 +0200

    Added dynamic generation of a manifest to make use of theming capabilities in modern browsers. Implemented some cahnges suggested by @sbs20

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -95,23 +95,25 @@ export default {
       description : 'SANE scanner nodejs web ui',
       icons : [
         {
-          src : '/icons/android-chrome-192x192.png',
+          src : './icons/android-chrome-192x192.png',
           sizes : '192x192',
-          type : 'image/png'
+          type : 'image/png',
+          purpose : 'any'
         },
         {
-          src : '/icons/android-chrome-512x512.png',
+          src : './icons/android-chrome-512x512.png',
           sizes : '512x512',
           type : 'image/png',
+          purpose : 'any'
         },
         {
-          src : '/icons/android-chrome-maskable-192x192.png',
+          src : './icons/android-chrome-maskable-192x192.png',
           sizes : '192x192',
           type : 'image/png',
           purpose : 'maskable'
         },
         {
-          src : '/icons/android-chrome-maskable-512x512.png',
+          src : './icons/android-chrome-maskable-512x512.png',
           sizes : '512x512',
           type : 'image/png',
           purpose : 'maskable'
