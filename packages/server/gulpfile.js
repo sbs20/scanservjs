057b49717cab14ee1491ffdc954d6fc7ed39df16
Marie Ramlow <me@nycode.dev>
2023-08-04 23:11:24 +0200

    migrate to npm workspaces

--- packages/server/gulpfile.js
+++ packages/server/gulpfile.js
@@ -35,7 +35,6 @@ const app = {
         './installer.sh',
         './scanservjs.service',
         './package.json',
-        './package-lock.json',
         './*config/**/config.default.js',
         './*data/**/default.jpg'])
         .pipe(shellFilter)
