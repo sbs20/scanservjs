6795d727459dd556507882d73c469cd3c88582da
Marie Ramlow <me@nycode.dev>
2023-09-20 15:44:22 +0200

    improve system endpoint

--- packages/server/src/classes/system.js
+++ packages/server/src/classes/system.js
@@ -1,11 +1,17 @@
 const os = require('os');
+const fs = require('fs/promises');
 const Process = require('./process');
+
+// Detect npm version and container type and cache them here
+const npmVersion = getNpmVersion();
+const containerType = detectContainerType();
 
 module.exports = class System {
   /**
    * @returns {Promise.<SystemInfo>}
    */
   async info() {
+    const container = await containerType;
     const info = {
       os: {
         arch: os.arch(),
@@ -15,11 +21,9 @@ module.exports = class System {
         type: os.type()
       },
       node: process.version,
-      npm: (await Process.spawn('npm -v')).toString().trim(),
-      docker: (await Process.spawn(
-        'grep -s \'docker\\|lxc\' /proc/1/cgroup',
-        undefined,
-        { ignoreErrors: true } )).length > 0
+      npm: await npmVersion,
+      docker: container === "docker",
+      containerType: container,
     };
 
     try {
@@ -31,3 +35,44 @@ module.exports = class System {
     return info;
   }
 };
+
+/**
+ * @returns {Promise<string | null>} The detected npm version, or null, if detection failed.
+ */
+async function getNpmVersion() {
+  try {
+    const proc = await Process.spawn('npm -v');
+    return proc.toString().trim();
+  } catch (error) {
+    console.error("Failed to determine npm version", error);
+    return null;
+  }
+}
+
+/**
+ * @returns {Promise<'docker' | 'podman' | 'systemd-nspawn' | null>} The used container technology, or null if no container was detected.
+ */
+async function detectContainerType() {
+  try {
+    // Detect docker by the /.dockerenv file
+    await fs.stat('/.dockerenv');
+    return 'docker';
+  } catch (_) {}
+
+  try {
+    // Detect podman by the /run/.containerenv file
+    await fs.stat('/run/.containerenv');
+    return 'podman';
+  } catch (_) {}
+
+  try {
+    // Detect systemd-nspawn by the /run/host/container-manager file
+    const manager = await fs.readFile("/run/host/container-manager", 'utf-8');
+    if (manager === 'systemd-nspawn') {
+      return 'systemd-nspawn';
+    }
+  } catch (_) {}
+
+  return null;
+}
+
